1267f74b1b9f2c68799660e6cac309997c73d90d
Dan Andersen & Jenna Espezua <pair+dandersen2.espezua@devbootcamp.com>
2016-02-12 16:54:27 -0500

    Update answers, comments, and routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,26 @@
 class CommentsController < ApplicationController
 
-  if params[:comentable_type] == "Question"
-    @commentable = Question.find(params[:commentable_id])
-  elsif
+  def new
+    @comment = Comment.new
+    # @commentable =
+  end
 
+  def create
+    if params[:comentable_type] == "Question"
+      @commentable = Question.find(params[:commentable_id])
+    elsif params[:commentable_type]  == 'Answer'
+      @commentable = Answer.find(params[:commentable_id])
+    end
 
+    @comment = @commentable.comments.build(comment_params)
+      if @comment.save
+        redirect_to
+      else
+      end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content).merge(commenter: current_user)
+    end
 end
