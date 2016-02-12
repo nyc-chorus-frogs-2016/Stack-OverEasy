1eb624bf097b196ee2162747840c6e2f36630b57
Scott Chou <iscott.chou@gmail.com>
2016-02-12 12:28:00 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: /@/}, uniqueness: true
+
   has_many :questions, foreign_key: 'questioner_id'
   has_many :answers, foreign_key: 'responder_id'
   has_many :votes, foreign_key: 'voter_id'
   has_many :comments, foreign_key: 'commenter_id'
 
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
