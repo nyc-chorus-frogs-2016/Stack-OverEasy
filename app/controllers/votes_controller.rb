af027f39d36dc7738ab6e833cc00ab63cce3a44d
Scott Chou <iscott.chou@gmail.com>
2016-02-12 17:33:40 -0500

    Start votes controller and question index page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,25 @@
 class VotesController < ApplicationController
+
+  def create
+    vote = Vote.new(vote_params)
+    binding.pry
+    if vote.save
+      if request.xhr?
+        {vote}.to_json
+      else
+        redirect_to question_path
+      end
+    else
+      return [507, "Your upvote on this post could not be saved, we're sorry"]
+    end
+
+  end
+
+
+  private
+
+  def vote_params
+    params.require(:vote).permit(:value, :voter_id, :votable_type, :votable_id)
+  end
+
 end
