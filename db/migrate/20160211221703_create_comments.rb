52f6a8b429713994e4a5028c3eb1eb96ff41b90b
Scott Chou <iscott.chou@gmail.com>
2016-02-12 10:21:03 -0500

    Add Index True to all migrations, and add best answer method to question model

--- db/migrate/20160211221703_create_comments.rb
+++ db/migrate/20160211221703_create_comments.rb
@@ -1,10 +1,13 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.integer :commentable_id
-      t.integer :commenter_id
-      t.string :commentable_type
       t.string :content
+      t.references :commenter, index: true
+
+      t.references :commentable, polymorphic: true, index: true
+      # t.string :commentable_type
+      # t.integer :commentable_id, index: true
+
 
       t.timestamps null: false
     end
