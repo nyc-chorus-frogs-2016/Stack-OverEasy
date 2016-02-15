f9dd3eb0887d7c6c75829416894ccb2b6b140282
Dan Andersen & Jenna Espezua & Scott Chou <pair+dandersen2.espezua.scottychou@devbootcamp.com>
2016-02-15 09:21:35 -0500

    Fix user single-vote constraint

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,22 +7,24 @@ class VotesController < ApplicationController
       @votable = Question.find(params[:question_id])
     end
 
-    @vote = @votable.votes.build(value: params[:value], voter: current_user)
-    if @vote.save
-      if request.xhr?
-        render :json => {votable_id: @votable.id, votable_vote_count: @votable.vote_count}
+    if !@votable.voted.include?(current_user.id)
+      @vote = @votable.votes.build(value: params[:value], voter: current_user)
+      if @vote.save
+        if request.xhr?
+          render :json => {votable_id: @votable.id, votable_vote_count: @votable.vote_count}
+        else
+          if params[:answer_id]
+            redirect_to question_path(@vote.votable.question_id)
+          else
+            redirect_to question_path(@vote.votable)
+          end
+        end
       else
         if params[:answer_id]
-          redirect_to question_path(@vote.votable.question_id)
+           render question_path(@vote.votable.question_id)
         else
-          redirect_to question_path(@vote.votable)
+           render question_path(@vote.votable)
         end
-      end
-    else
-      if params[:answer_id]
-         render question_path(@vote.votable.question_id)
-      else
-         render question_path(@vote.votable)
       end
     end
   end
